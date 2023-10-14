be9af4bbe40d2c5fa87596cd652c87cbdf5489fa
Logan Finnegan <logantfinnegan@protonmail.com>
2023-10-14 13:46:10 -0500

    Add test for node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,21 @@
-require ".lib/node"
+require "./lib/node"
+# require "rspec"
+# require "pry"; binding.pry
+
+RSpec.describe Node do
+    it 'exists' do
+        node = Node.new('plop')
+        expect(node).to be_instance_of(Node)
+    end
+
+    it "has data" do
+        node = Node.new('plop')
+        expect(node.data).to eq('plop')
+    end
+
+    it "has next node" do
+        node = Node.new('plop')
+        expect(node.next_node).to eq(nil)
+    end
+end
+
